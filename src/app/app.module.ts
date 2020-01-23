f7cd16f299dc31d994b0b1eb004d4dcf4c8f0322
Markus Fantone <markusfantone@hotmail.com>
2020-01-23 12:53:41 -0300

    Adding Angular-Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { ListUrlComponent } from "../components/list-url/list-url.component";
 import { AppComponent } from "./app.component";
+import { MatInputModule } from "@angular/material/input"
 
 @NgModule({
   declarations: [AppComponent, ListUrlComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, MatInputModule],
   providers: [],
   bootstrap: [AppComponent]
 })
